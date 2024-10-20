165b8612213578c31e6ca6ea83a9a67fb597550d
Tickether <tickether@gmail.com>
2024-10-20 15:08:36 +0000

    change format

--- components/dashboard/component.tsx
+++ components/dashboard/component.tsx
@@ -39,7 +39,7 @@ export function Component () {
         const oneJan = new Date(year, 0, 1);
         const dayOfYear = Math.ceil((date.getTime() - oneJan.getTime()) / 86400000);
         const weekNumber = Math.ceil((dayOfYear + oneJan.getDay() + 1) / 7);
-        return `${year}-${String(weekNumber).padStart(2, '0')}`;
+        return `${String(weekNumber).padStart(2, '0')}, ${year}`;
     };
 
     async function sendWeeklyInvoices() {
