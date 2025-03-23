29c91c811e32dc840f5ccda645b3b024103189e1
Tickether <tickether@gmail.com>
2025-03-23 20:19:51 +0000

    fix: update Schemas

--- utils/constants/addresses.ts
+++ utils/constants/addresses.ts
@@ -1,8 +1,8 @@
 
 export const attester: `0x${string}` = "0x56aD1c5746cdFCfA1E0B7960b9A95BECb57dF6f8"
-export const ownerPinkSlipSchemaID: `0x${string}` = "0x47c"
-export const hirePurchaseSchemaID: `0x${string}` = "0xdbe"
-export const hirePurchaseInvoiceSchemaID: `0x${string}` = "0x199d"
-export const memberBadgeSchemaID: `0x${string}` = "0x609"
-export const hirePurchaseCreditScoreSchemaID: `0x${string}` = "0x17bc"
+export const ownerPinkSlipSchemaID: `0x${string}` = "0x2558"
+export const hirePurchaseSchemaID: `0x${string}` = "0x255c"
+export const hirePurchaseInvoiceSchemaID: `0x${string}` = "0x2567"
+export const memberBadgeSchemaID: `0x${string}` = "0x253d"
+export const hirePurchaseCreditScoreSchemaID: `0x${string}` = "0x256f"
 //export const ownerPinkSlipSchemaData: SchemaItem[] = [{"name":"vin","type":"string"},{"name":"make","type":"string"},{"name":"model","type":"string"},{"name":"year","type":"string"},{"name":"color","type":"string"},{"name":"country","type":"string"},{"name":"visualProof","type":"string[]"},{"name":"ownerProof","type":"string"},{"name":"transferProof","type":"string"}]
